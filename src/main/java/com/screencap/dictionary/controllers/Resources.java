3f21eb5fa427c5c91258afb77e6f83d2c71f024d
James Lee <machingclee@gmail.com>
2020-08-13 11:24:26 +0800

    test trigger

--- src/main/java/com/screencap/dictionary/controllers/Resources.java
+++ src/main/java/com/screencap/dictionary/controllers/Resources.java
@@ -11,6 +11,6 @@ public class Resources {
     @GetMapping
     public String getResources() {
         System.out.println("hello");
-        return "Hello World888";
+        return "Hello World444";
     }
 }
